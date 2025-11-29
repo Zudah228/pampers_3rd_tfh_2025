b1e172c17dc28169eae5f897bf4460bfa7050ff4
serizawa-T <86996275+serizawa-T@users.noreply.github.com>
2025-11-29 23:54:16 +0900

    RoomJoinCard の追加

--- lib/features/room/components/room_join_card.dart
+++ lib/features/room/components/room_join_card.dart
@@ -1,16 +1,50 @@
+import 'package:app/core/app/components/button/primary_button.dart';
+import 'package:app/features/room/use_cases/join_room_use_case.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RoomJoinCard extends ConsumerWidget {
+class RoomJoinCard extends ConsumerStatefulWidget {
   const RoomJoinCard({super.key});
+  @override
+  ConsumerState<RoomJoinCard> createState() => _RoomJoinCardState();
+}
+
+class _RoomJoinCardState extends ConsumerState<RoomJoinCard> {
+  final _controller = TextEditingController();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: SizedBox(
-          width: double.infinity,
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 8),
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'ルームIDを入力してください',
+                ),
+              ),
+              SizedBox(height: 24),
+              PrimaryButton(
+                onPressed: () {
+                  ref.read(joinRoomUseCaseProvider)(_controller.text);
+                },
+                child: Text('参加'),
+              ),
+            ],
+          ),
         ),
       ),
     );
