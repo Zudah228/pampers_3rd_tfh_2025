da0d16ff7eab2963afff3792292d6a23f55b5ca9
serizawa <86996275+serizawa-T@users.noreply.github.com>
2025-11-30 02:12:51 +0900

    ルームに参加する機能 RoomJoinCard の追加 (#6)
    * RoomJoinCard の追加
    
    * RoomJoin時のバリデーションを追加
    
    * const を追加

--- lib/features/room/components/room_join_card.dart
+++ lib/features/room/components/room_join_card.dart
@@ -1,16 +1,60 @@
+import 'package:app/core/app/components/button/primary_button.dart';
+import 'package:app/core/app/components/full_screen_loading_indicator.dart';
+import 'package:app/core/exceptions/message_exception.dart';
+import 'package:app/features/room/use_cases/join_room_use_case.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RoomJoinCard extends ConsumerWidget {
+class RoomJoinCard extends ConsumerStatefulWidget {
   const RoomJoinCard({super.key});
+  @override
+  ConsumerState<RoomJoinCard> createState() => _RoomJoinCardState();
+}
+
+class _RoomJoinCardState extends ConsumerState<RoomJoinCard> {
+  final _controller = TextEditingController();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: SizedBox(
-          width: double.infinity,
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 8),
+              TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'ルームIDを入力してください',
+                ),
+              ),
+              SizedBox(height: 24),
+              PrimaryButton(
+                onPressed: () {
+                  FullScreenLoadingIndicator.show(() async {
+                    final roomId = _controller.text.trim();
+
+                    if (roomId.isEmpty) {
+                      throw MessageException('ルームIDを入力してください');
+                    }
+
+                    await ref.read(joinRoomUseCaseProvider)(roomId);
+                  });
+                },
+                child: Text('参加'),
+              ),
+            ],
+          ),
         ),
       ),
     );
