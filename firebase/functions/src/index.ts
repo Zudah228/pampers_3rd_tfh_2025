64cc7ab6ba571cfdc7a74ac52ba2de802e693605
づだ <62398366+Zudah228@users.noreply.github.com>
2025-11-30 11:58:37 +0900

    compareFaces の Callable 関数の実装 (#9)
    * compareFaces の実装
    
    * パラメータの変更
    
    * アップデート
    
    * Update arguments
    
    * 不要な functions.config() の削除
    
    * Add aws key
    
    * SimilarityThreshold の追加
    
    * エラー種別の変更
    
    * Add userIds filter
    
    * Swap  userImage and groupImage
    
    * エラー種別の変更
    
    * jsdoc の linter の削除
    
    ---------
    
    Co-authored-by: kt-hayashi <kt-hayashi@lermo.co.jp>

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,9 +1,10 @@
+import * as aws from "aws-sdk"
 import { initializeApp } from "firebase-admin/app"
 import { getFirestore } from "firebase-admin/firestore"
 import { getStorage } from "firebase-admin/storage"
 import * as functions from "firebase-functions/v2"
 
-initializeApp(functions.config().firebase)
+initializeApp()
 
 const firestore = getFirestore()
 firestore.settings({
@@ -13,9 +14,43 @@ firestore.settings({
 
 const storage = getStorage()
 
-functions.setGlobalOptions({ region: "asia-northeast1" });
+functions.setGlobalOptions({ region: "asia-northeast1", secrets: ["AWS_SECRET_KEY", "AWS_PUBLIC_KEY"] })
 
-export const saveFaceIds = functions.https.onCall(async (request) => {
+const download = async (filePath: string): Promise<Buffer> => {
+    const [response] = await storage.bucket().file(filePath).get()
+    const [bufferResponse] = await response.download()
+    const buffer = bufferResponse.buffer
+    return Buffer.from(buffer)
+}
+
+const secretAccessKey = functions.params.defineSecret("AWS_SECRET_KEY").value()
+const accessKeyId = functions.params.defineSecret("AWS_PUBLIC_KEY").value()
+
+aws.config.update({
+    accessKeyId,
+    secretAccessKey,
+    region: "us-east-1",
+})
+
+const awsRekognition = new aws.Rekognition()
+
+const _compareFaces = async (sourceImage: Buffer, targetImage: Buffer) => {
+    const response = await awsRekognition
+        .compareFaces({
+            SourceImage: {
+                Bytes: sourceImage,
+            },
+            TargetImage: {
+                Bytes: targetImage,
+            },
+            SimilarityThreshold: 80,
+        })
+        .promise()
+
+    return response
+}
+
+export const compareFaces = functions.https.onCall(async (request) => {
     const { auth, data } = request
 
     if (auth == null) {
@@ -25,45 +60,48 @@ export const saveFaceIds = functions.https.onCall(async (request) => {
     const filePath = data.file_path
     const roomId = data.room_id
 
-    if (!filePath || !roomId) {
-        throw new functions.https.HttpsError("resource-exhausted", "file_path,roomId が必要です")
+    if (!filePath || typeof filePath !== "string" || !roomId || typeof roomId !== "string") {
+        throw new functions.https.HttpsError("invalid-argument", "file_path,roomId が必要です")
     }
 
-    const response = await storage.bucket().file(filePath).get()
-    // TODO: Azure 経由で顔の ID をとってきて、face_ids を返す
-    response
+    const groupImage = await download(filePath)
 
-    const face_ids: string[] = []
+    const users = await firestore.collectionGroup("related_rooms").where("room_id", "==", roomId).get()
 
-    await firestore.doc(`room/${roomId}`).update({
-        face_ids,
-    })
+    const hasUsers = users.docs.length > 0
+    const userIds = users.docs
+        .map((doc) => doc.data()?.user_id)
+        .filter((userId): userId is string => typeof userId === "string")
 
+    const userImages = await Promise.all(
+        userIds.map(async (userId) => {
+            const imagePath = await firestore.collection("users").doc(userId).get()
+
+            const avatarPath = imagePath.data()?.avatar_path
+            if (!avatarPath) {
+                throw new functions.https.HttpsError(
+                    "invalid-argument",
+                    "avatar_path が設定されていないユーザーがいます"
+                )
+            }
+
+            return download(avatarPath)
+        })
+    )
+
+    const compareFacesResponses = await Promise.all(
+        userImages.map(async (userImage) => {
+            return _compareFaces(userImage, groupImage)
+        })
+    )
+
+    // すべてのユーザーが顔が、１人以上と一致しているかどうかを判断
+    const allMatched = compareFacesResponses.every((response) =>
+        response.FaceMatches ? response.FaceMatches.length > 0 : false
+    )
+
+    const success = allMatched && hasUsers
     return {
-      face_ids
-    }
-})
-
-export const fetchFaceIds = functions.https.onCall(async (request) => {
-    const { auth, data } = request
-
-    if (auth == null) {
-        throw new functions.https.HttpsError("unauthenticated", "認証エラー")
-    }
-
-    const filePath = data.file_path
-
-    if (!filePath) {
-        throw new functions.https.HttpsError("resource-exhausted", "file_path が必要です")
-    }
-
-    const response = await storage.bucket().file(filePath).get()
-    // TODO: Azure 経由で顔の ID をとってきて、face_ids を返す
-    response
-
-    const face_ids: string[] = []
-
-    return {
-      face_ids
+        success,
     }
 })
