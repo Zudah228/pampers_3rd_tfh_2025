3f4c44fd9a8704e184dc0fbf91a37e265f9636a1
Zudah228 <rezel95.hin@gmail.com>
2025-11-30 14:09:20 +0900

    compareFacesUseCaseProvider を組み込む

--- lib/features/unlock/pages/unlock_page.dart
+++ lib/features/unlock/pages/unlock_page.dart
@@ -1,19 +1,25 @@
 import 'package:app/core/app/components/button/primary_button.dart';
 import 'package:app/core/app/components/full_screen_loading_indicator.dart';
 import 'package:app/core/app/components/route_animations/route_animations.dart';
+import 'package:app/core/app/components/snack_bar.dart';
 import 'package:app/features/photo/pages/album_page.dart';
+import 'package:app/features/room/use_cases/compare_faces_use_case.dart';
 import 'package:app/features/unlock/providers/unlock_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UnlockPage extends ConsumerStatefulWidget {
-  const UnlockPage({super.key});
+  const UnlockPage({super.key, required this.roomId});
+
+  final String roomId;
+
   static const routeName = '/unlock';
-  static Route<void> route() {
+
+  static Route<void> route(String roomId) {
     return RouteAnimations.swipeBack<void>(
       settings: const RouteSettings(name: routeName),
-      builder: (_) => const UnlockPage(),
+      builder: (_) => UnlockPage(roomId: roomId),
     );
   }
 
@@ -155,14 +161,16 @@ class _UnlockPageState extends ConsumerState<UnlockPage>
 
                   final image = await pickedImage.readAsBytes();
 
-                  // TODO: 撮影した画像を処理する。
-                  // 画像の解析を行い、二人が写っているかどうかを判断する。
-                  // 二人が本人かどうかの解析を行う
-                  // 解析が成功した場合は、ロック解除する。
-                  // 解析が失敗した場合は、エラーメッセージを表示する。
-
-                  //現在は画像投稿されたかのみを判定
                   await FullScreenLoadingIndicator.show(() async {
+                    final checked = await ref.read(compareFacesUseCaseProvider)(
+                      data: image,
+                      roomId: widget.roomId,
+                    );
+
+                    if (!checked) {
+                      showErrorSnackBar(error: '違う人が写っていますね');
+                      return;
+                    }
                     await ref.read(unlockProvider).unlock(image);
                   });
 
